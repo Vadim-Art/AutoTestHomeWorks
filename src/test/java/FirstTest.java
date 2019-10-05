4de1ce18c0d99fdf6d80b5686024fc16fcf5071f
Vadim Artemenko <artemenko.vadim@gmail.com>
2019-10-05 18:58:23 +0300

    Added Xpath locators

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -12,37 +12,49 @@ import org.testng.annotations.Test;
 
 public class FirstTest {
 
-    private static WebDriver driver;
+    private static ChromeDriver driver;
+    private WebDriverWait wait;
 
     private String expectedName = "testautotest";
     private String expectedUrl = "https://github.com";
 
-    private By usernameInput = By.id("login_field");
-    private By passwordInput = By.id("password");
-    private By singinButton = By.cssSelector(".auth-form-body [name='commit']");
-    private By dropdown = By.cssSelector(".details-overlay .Header-link[aria-label*='View profile']");
-    private By targetName = By.cssSelector(".header-nav-current-user .css-truncate-target");
+    //private By usernameInput = By.id("login_field");
+    private By usernameInput = By.xpath("//input[@id='login_field']");
+    //private By passwordInput = By.id("password");
+    private By passwordInput = By.xpath("//input[@id='password']");
+    //private By singinButton = By.cssSelector(".auth-form-body [name='commit']");
+    private By signinButton = By.xpath("//input [@type = 'submit']");
+    //private By dropdown = By.cssSelector(".details-overlay .Header-link[aria-label*='View profile']");
+    private By dropdown = By.xpath("//summary [contains(@aria-label, 'View profile')]");
+    //private By targetName = By.cssSelector(".header-nav-current-user .css-truncate-target");
+    private By targetName = By.xpath("//strong [contains(., 'test')]");
+    //private By visibleDropdown = By.cssSelector(".details-overlay .dropdown-menu.mt-2");
+    private By visibleDropdown = By.xpath("//details-menu [contains (@class,'mt-2')]");
+    //private By loginForm = By.cssSelector(".auth-form .auth-form-body");
+    private By loginForm = By.xpath("//div[@class = 'auth-form-body mt-3']");
 
 
     @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "lib/drivers/chrome/chromedriver.exe");
-        ChromeOptions capabilities = new ChromeOptions();
-        driver = new ChromeDriver(capabilities);
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
+
         driver.get("https://github.com/login");
     }
 
 
     @Test
     public void userLogin() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginForm));
         driver.findElement(usernameInput).sendKeys("testautotest12345@gmail.com");
         driver.findElement(passwordInput).sendKeys("qwerty12345auto");
-        driver.findElement(singinButton).click();
+        driver.findElement(signinButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dropdown));
         driver.findElement(dropdown).click();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement element = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".details-overlay .dropdown-menu.mt-2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(visibleDropdown));
 
         Assert.assertTrue(driver.getCurrentUrl().contains(expectedUrl));
 
