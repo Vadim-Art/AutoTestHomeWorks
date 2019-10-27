6b2cb6b4da37b5f17ee5f514c9e067ffeba439c2
Vadim Artemenko <artemenko.vadim@gmail.com>
2019-10-27 11:21:04 +0200

    Solved issue with property to run the test

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    ChromeDriver driver;
+
+    @BeforeSuite
+    public void before() {
+        File chromeDriver = new File(System.getProperty("chrome.driver.path"));
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(chromeDriver)
+                .usingAnyFreePort()
+                .build();
+        ChromeOptions options = new ChromeOptions().addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(service, options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/ViewerTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class ViewerTest extends TestBase {
+
+    private WebDriverWait wait;
+
+    private String expectedName = "testautotest";
+    private String expectedUrl = "https://github.com";
+
+    private By usernameInput = By.xpath("//input[@id='login_field']");//By.id("login_field");
+    private By passwordInput = By.xpath("//input[@id='password']"); //By.id("password");
+    private By signinButton = By.xpath("//input [@type = 'submit']");//By.cssSelector(".auth-form-body [name='commit']");
+    private By dropdown = By.xpath("//summary [contains(@aria-label, 'View profile')]"); //By.cssSelector(".details-overlay .Header-link[aria-label*='View profile']");
+    private By targetName = By.xpath("//strong [contains(., 'test')]");//By.cssSelector(".header-nav-current-user .css-truncate-target");
+    private By visibleDropdown = By.xpath("//details-menu [contains (@class,'mt-2')]");// By.cssSelector(".details-overlay .dropdown-menu.mt-2");
+    private By loginForm = By.xpath("//div[@class = 'auth-form-body mt-3']");//By.cssSelector(".auth-form .auth-form-body");
+
+    @Test
+    public void userLogin() {
+        driver.get("https://github.com/login");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginForm));
+        driver.findElement(usernameInput).sendKeys("testautotest12345@gmail.com");
+        driver.findElement(passwordInput).sendKeys("qwerty12345auto");
+        driver.findElement(signinButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dropdown));
+        driver.findElement(dropdown).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(visibleDropdown));
+
+        Assert.assertTrue(driver.getCurrentUrl().contains(expectedUrl));
+
+        String actualUsername = driver.findElement(targetName).getText();
+
+        Assert.assertEquals(actualUsername, expectedName);
+    }
+}
